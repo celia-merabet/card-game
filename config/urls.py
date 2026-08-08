1928bbcdfe92d995205734cd648acbde73ffd0b8
celia-merabet <149301030+celia-merabet@users.noreply.github.com>
2026-08-08 17:53:50 +0200

    modif

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-
-from django.urls import path, include
-
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
 
 
 urlpatterns = [
@@ -9,6 +8,12 @@ urlpatterns = [
     path(
         "admin/",
         admin.site.urls
+    ),
+
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(),
+        name="logout"
     ),
 
     path(
